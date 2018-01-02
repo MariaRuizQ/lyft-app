e79b75e1c1312c97610738354280962db2c8d709
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2018-01-01 21:56:05 -0500

    Actializando js y html

---
+++ js/alert.js
@@ -0,0 +1,18 @@
+console.log('hola');
+$(document).ready(function(){
+  $('#button-code').click(function(){
+    function generated_code(character,length){
+      code = '';
+      for (i = 0; i < length; i++){
+        generate = Math.floor(Math.random()*character.length);
+        code += character.substr(generate, 1);
+      };
+      return code;
+    };
+    x = '0123456789';
+    y = 3;
+
+    var generated = $('#code').text('Tu código es: '+ 'LAB' + generated_code(x,y));
+    console.log(generated);
+  });
+});
---
+++ js/app.js
@@ -0,0 +1,3 @@
+/*haciendo la función del evento click al botón sing up*/
+
+/*haciendo la función del evento para ingresar el número de celular*/
