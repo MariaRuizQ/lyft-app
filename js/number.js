3ef9f72507b7e5a1eda428e840941c4acbda41f6
MariaRuizQ <ruiz.maria.quispe@gmail.com>
2018-01-02 12:48:23 -0500

    Añadiendo actualización antes 1:00pm!

--- js/number.js
+++ js/number.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
   var clickImg = $('li a img');
   clickImg.on('click',function(){
     $('#option-img').attr('src',$(this).attr('src'));
-
-  //  $('#number').text($(this).attr('data-country')));
   });
 
   var number = $('#number');
